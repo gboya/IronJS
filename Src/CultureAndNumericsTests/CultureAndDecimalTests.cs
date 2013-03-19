bb3f6fa7448e3ae77b352c49555bbe5d21ae5649
gboya <gabrielboya@gmail.com>
2013-03-19 11:22:10 +0100

    Clearer tests related to Culture and Numbers

--- Src/CultureAndNumericsTests/CultureAndDecimalTests.cs
+++ Src/CultureAndNumericsTests/CultureAndDecimalTests.cs
@@ -11,6 +11,9 @@ namespace CultureAndNumericsTests
     [TestClass]
     public class CultureAndDecimalTests
     {
+        /// <summary>
+        /// This Fails. See discussion of issue #97 on fholm/IronJS
+        /// </summary>
         [TestMethod]
         public void TestDecimalWithCultureInfo()
         {
@@ -24,6 +27,9 @@ namespace CultureAndNumericsTests
             Assert.AreEqual(decimalValue, returnedDecimal);
         }
 
+        /// <summary>
+        /// This Fails. See discussion of issue #97 on fholm/IronJS
+        /// </summary>
         [TestMethod]
         public void TestFloatWithCultureInfo()
         {
@@ -34,6 +40,9 @@ namespace CultureAndNumericsTests
             Assert.IsTrue(result, "1.5 should be smaller");
         }
 
+        /// <summary>
+        /// This passes.
+        /// </summary>
         [TestMethod]
         public void TestDecimalWithCultureInfoEnUS()
         {
@@ -42,6 +51,59 @@ namespace CultureAndNumericsTests
             ctx.SetGlobal("decimalVal", 1.5m);
             var result = ctx.Execute("decimalVal < 3");
             Assert.IsTrue(result, "1.5 should be smaller");
+        }
+
+        /// <summary>
+        /// This passes, since decimal is boxed and unboxed as a plain CLR Object.
+        /// </summary>
+        [TestMethod]
+        public void TestDecimalEqualityAfterSetAndGet()
+        {
+            var ctx = new IronJS.Hosting.CSharp.Context();
+            const decimal d = 1.5m;
+            const string globalVariableName = "decimalVal";
+            
+            ctx.SetGlobal(globalVariableName, d);
+            var result = ctx.GetGlobalAs<decimal>(globalVariableName);
+
+            Assert.AreEqual(d, result);
+        }
+
+        /// <summary>
+        /// This passes as single is implicitly casted to double.
+        /// </summary>
+        [TestMethod]
+        public void IsCLRSingleBoxedIntoNumber()
+        {
+            var boxedSingle = IronJS.Runtime.BoxedValue.Box(1.5F);
+            Assert.IsTrue(boxedSingle.IsNumber);
+        }
+
+        /// <summary>
+        /// This fails.
+        /// </summary>
+        [TestMethod]
+        public void IsCLRSingleAnIronJSNumber()
+        {
+            var ctx = new IronJS.Hosting.CSharp.Context();
+            const float f = 1.5F;
+
+            ctx.SetGlobal("myFloat", f);
+            var result = ctx.GetGlobal("myFloat");
+
+            Assert.IsTrue(result.IsNumber);
+        }
+
+        [TestMethod]
+        public void IsCLRDoubleAnIronJSNumber()
+        {
+            var ctx = new IronJS.Hosting.CSharp.Context();
+            const double d = 1.5;
+
+            ctx.SetGlobal("myDouble", d);
+            var result = ctx.GetGlobal("myDouble");
+
+            Assert.IsTrue(result.IsNumber);
         }
     }
 }
